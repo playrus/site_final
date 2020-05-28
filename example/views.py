5681a2d0485044fe22ec191e401743fd10a5e1e7
Anton <2playrus@gmail.com>
2020-05-28 21:01:02 +0300

    edit main page

--- example/views.py
+++ example/views.py
@@ -13,5 +13,7 @@ def index(request):
 
 
 def home(request):
-    products_images=ProductImage.objects.filter(is_main=True)
+    products_images=ProductImage.objects.filter(is_main=True, product__is_active=True)
+    products_images_phones = products_images.filter(product__category__id=1)
+    products_images_laptops = products_images.filter(product__category__id=2)
     return render(request, "landing/home.html", locals())
