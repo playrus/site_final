21d55806662d87facaa108e0076fde15f635decb
Anton <2playrus@gmail.com>
2020-06-02 02:44:38 +0300

    add checkout(only frontend)

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
-from django.http import JsonResponse
-from .models import ProductInBasket
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from .models import *
+from django.shortcuts import render
+from .forms import CheckoutContactForm
+from django.contrib.auth.models import User
 
 
 def basket_adding(request):
@@ -14,14 +17,15 @@ def basket_adding(request):
     if is_delete == 'true':
         ProductInBasket.objects.filter(id=product_id).update(is_active=False)
     else:
-        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, is_active=True, defaults={"nmb": nmb})
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
+                                                                     is_active=True, defaults={"nmb": nmb})
         if not created:
             print ("not created")
             new_product.nmb += int(nmb)
             new_product.save(force_update=True)
 
     #common code for 2 cases
-    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True, order__isnull=True)
     products_total_nmb = products_in_basket.count()
     return_dict["products_total_nmb"] = products_total_nmb
 
@@ -36,3 +40,44 @@ def basket_adding(request):
         return_dict["products"].append(product_dict)
 
     return JsonResponse(return_dict)
+
+
+def checkout(request):
+    session_key = request.session.session_key
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True, order__isnull=True)
+    print (products_in_basket)
+    for item in products_in_basket:
+        print(item.order)
+
+
+    form = CheckoutContactForm(request.POST or None)
+    if request.POST:
+        print(request.POST)
+        if form.is_valid():
+            print("yes")
+            data = request.POST
+            name = data.get("name", "3423453")
+            phone = data["phone"]
+            user, created = User.objects.get_or_create(username=phone, defaults={"first_name": name})
+
+            order = Order.objects.create(user=user, customer_name=name, customer_phone=phone, status_id=1)
+
+            for name, value in data.items():
+                if name.startswith("product_in_basket_"):
+                    product_in_basket_id = name.split("product_in_basket_")[1]
+                    product_in_basket = ProductInBasket.objects.get(id=product_in_basket_id)
+                    print(type(value))
+
+                    product_in_basket.nmb = value
+                    product_in_basket.order = order
+                    product_in_basket.save(force_update=True)
+
+                    ProductInOrder.objects.create(product=product_in_basket.product, nmb = product_in_basket.nmb,
+                                                  price_per_item=product_in_basket.price_per_item,
+                                                  total_price = product_in_basket.total_price,
+                                                  order=order)
+
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        else:
+            print("no")
+    return render(request, 'orders/checkout.html', locals())
